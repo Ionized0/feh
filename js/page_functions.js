aa45df49b0f84e7c8c1402add430006c2e240931
Jonathan Vukim <jonny.vukim@gmail.com>
2017-11-16 14:25:28 +1000

    Added some layout styles to divs, created initial footer

--- js/page_functions.js
+++ js/page_functions.js
@@ -3,5 +3,29 @@ function createHeader() {
 }
 
 function createFooter() {
-	
+	var sFooterId = "#idFooter";
+	var oFooter = $(sFooterId);
+
+	var sAuthor = "Jonathan Vukim"
+	var sDate = getFormattedDate();
+	var oAuthor = $("<div>", {
+		html: sAuthor,
+		class: "col",
+		style: "text-align: left"
+	});
+	var oDate = $("<div>", {
+		html: sDate,
+		class: "col",
+		style: "text-align: right"
+	});
+	oFooter.append(oAuthor);
+	oFooter.append(oDate);
+}
+
+function getFormattedDate() {
+	var oDate = new Date();
+	var iDate = oDate.getDate();
+	var iMonth = oDate.getMonth() + 1;
+	var iYear = oDate.getFullYear();
+	return iDate + "/" + iMonth + "/" + iYear;
 }

38324a7efa81a98141c91dc3e6ee85fa3d598dc7
Jonathan Vukim <jonny.vukim@gmail.com>
2017-11-17 16:26:19 +1000

    Added functionality for a bar chart, added css styling elements for that

--- js/index_functions.js
+++ js/index_functions.js
@@ -69,11 +69,7 @@ function resetTemplate() {
 // Visualises Stat vs. Number of Characters
 function createGraph(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
 	console.log(oInfo);
-	var sId = "idChart";
-	var oSvg = $("<svg>", {
-		id: sId
-	});
-	$("#idContent").append(oSvg);
+	var oContent = $("#idContent");
 
 	var sGraphType = getGraphType();
 	var sStatFilter = getStatFilter();
@@ -85,25 +81,49 @@ function createGraph(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
 		bottom: 30,
 		left: 40
 	},
-		width = 960 - margin.left - margin.right;
-		height = 500 - margin.top - margin.bottom;
+		width = oContent[0].clientWidth - margin.left - margin.right;
+		height = oContent[0].clientHeight - margin.top - margin.bottom;
 
-	var x = d3.scaleBand().rangeRound([0, width], 0.1);
+	var x = d3.scaleBand().range([0, width])
+		.padding(0.1);
 	var y = d3.scaleLinear().range([height, 0]);
 
-	var xAxis = d3.axisBottom()
-		.scale(x);
-
-	var yAxis = d3.axisLeft()
-		.scale(y);
-
-	var oChart = d3.select("#idChart")
+	var oChart = d3.select("#idContent").append("svg")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom)
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var o
+	var oDataset = configureDataset(oInfo, sGraphType, sStatFilter, aAttributeFilters);
+	console.log(oDataset);
+
+	var xDomain = getXDomain(oDataset);
+	x.domain(xDomain);
+	y.domain([0, d3.max(oDataset, function (d) { return d.y; })]);
+
+	oChart.selectAll(".bar")
+		.data(oDataset)
+		.enter().append("rect")
+		.attr("class", "bar")
+		.attr("x", function (d) { return x(d.x); })
+		.attr("y", function (d) { return y(d.y); })
+		.attr("height", function (d) { return height - y(d.y); })
+		.attr("width", x.bandwidth());
+
+	oChart.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x));
+
+	oChart.append("g")
+		.attr("class", "y axis")
+		.call(d3.axisLeft(y))
+		.append("text")
+		.attr("class", "axisHeading")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.style("text-anchor", "end")
+		.text("Number of Heroes");
 }
 
 function getGraphType() {
@@ -116,4 +136,39 @@ function getStatFilter() {
 
 function getAttributeFilters() {
 	return [];
+}
+
+function configureDataset(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
+	var oDataset = [];
+	oDataset.push({
+		x: oInfo[0][sStatFilter],
+		y: 0
+	});
+	switch(sGraphType) {
+		case "Bar":
+			oInfo.forEach(function (character) {
+				for (var i = 0; i < oDataset.length; i++) {
+					if(oDataset[i].x == character[sStatFilter]) {
+						oDataset[i].y++;
+						break;
+					} else if(i == oDataset.length - 1) {	// If not exists AND at end of loop
+						oDataset.push({
+							x: character[sStatFilter].toString(),
+							y: 0
+						});
+					}
+				}
+			});
+			return oDataset;
+		default:
+			break;
+	}
+}
+
+function getXDomain(oDataset) {
+	var aMap = [];
+	oDataset.forEach(function (entry) {
+		aMap.push(entry.x);
+	});
+	return aMap.sort();
 }

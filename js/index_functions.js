0b9db57064f10fddc0758f4f89bb85d1ea709d18
Jonathan Vukim <jonny.vukim@gmail.com>
2017-11-23 16:53:37 +1000

    Completed functionality for changing stat filters, added styling and colours to the chart

--- js/index_functions.js
+++ js/index_functions.js
@@ -8,6 +8,14 @@ var sNameParam = "HeroName",
 	sWeaponParam = "WeaponType",
 	sMovementParam = "MoveType";
 
+var oColours = {	// Red, Orange, Green, Yellow, Blue
+	"HP": "#FF2C0D",
+	"ATK": "#E8950C",
+	"SPD": "#0CE827",
+	"DEF": "#FFFA00",
+	"RES": "#00E2FF"
+};
+
 var stats = {
 	"HP": sHpParam,
 	"ATK": sAtkParam,
@@ -15,6 +23,8 @@ var stats = {
 	"DEF": sDefParam,
 	"RES": sResParam
 };
+
+var aDefaultSelectedStats = ["HP"];
 
 var attributes = {
 	"Name": sNameParam,
@@ -27,6 +37,8 @@ var filterTypes = [
 	"Attributes"
 ];
 
+var oFetchedInfo;
+
 function createContent() {
 	createFilterArea();
 	getInfo();
@@ -34,31 +46,67 @@ function createContent() {
 
 function createFilterArea() {
 	// Add both filter areas to content
-	var sFilterAreaClass = "filterArea row";
+	var sFilterAreaClass = "row";
 	var oFilterHeading = $("<h2>", {
-		html: "Filters"
+		html: "Filters",
+		class: "row"
 	});
 	var oStatFilterArea = $("<div>", {
 		id: "idStatFilters",
-		class: sFilterAreaClass,
+		class: "row",
 	});
 	var oAttributeFilterArea = $("<div>", {
 		id: "idAttributeFilters",
-		class: sFilterAreaClass
+		class: "row"
 	});
-	var oContent = $("#idContent");
+	var oContentArea = $("#idContent");
+	var oContent = $("<div>", {
+		class: "container"
+	});
 	oContent.append(oFilterHeading);
 	oContent.append(oStatFilterArea);
 	oContent.append(oAttributeFilterArea);
+	oContentArea.append(oContent);
 
 	// Stat filter
+	var oStatFilterContainer = $("<div>", {
+		class: "container"
+	});
 	var oStatHeading = $("<h4>", {
-		html: "Stats"
+		html: "Stats",
+		class: "row"
 	});
-	oStatFilterArea.append(oStatHeading);
+	var oStatFilters = $("<div>", {
+		class: "row"
+	});
 	Object.keys(stats).forEach(function (stat) {
-		console.log(stat);
+		// Create checkboxes and labels
+		var checked = false;
+		if (aDefaultSelectedStats.indexOf(stat) >= 0) {
+			checked = true;
+		}
+		var oContainer = $("<div>");
+		var oStatCheckbox = $("<input>", {
+			class: "checkbox stat",
+			type: "checkbox",
+			name: "stat",
+			value: stat,
+			checked: checked
+		}).click(function () {
+			changeStat(stat);
+		});
+		var oLabel = $("<span>", {
+			html: stat,
+			class: "label",
+		});
+
+		oContainer.append(oStatCheckbox)
+			.append(oLabel);
+		oStatFilters.append(oContainer);
 	});
+	oStatFilterContainer.append(oStatHeading);
+	oStatFilterContainer.append(oStatFilters);
+	oStatFilterArea.append(oStatFilterContainer);
 }
 
 function getInfo() {
@@ -74,7 +122,8 @@ function getInfo() {
 		dataType: "jsonp",
 		success: function(oData) {
 			oInfo = simplifyInfo(oData.query.results);
-			createGraph(oInfo);
+			oFetchedInfo = oInfo;
+			createGraph(oFetchedInfo);
 		}
 	});
 }
@@ -89,10 +138,10 @@ function simplifyInfo(oInfo) {
 		oCharacterInfo.Attributes.Name = name;
 		// Add stats to oCharacterInfo
 		Object.keys(stats).forEach(function (stat) {
-			oCharacterInfo[stat] = parseInt(sPrintoutPath[stats[stat]][0]);
+			oCharacterInfo["Stats"][stat] = parseInt(sPrintoutPath[stats[stat]][0]);
 		});
-		oCharacterInfo.BST = oCharacterInfo.HP + oCharacterInfo.ATK + oCharacterInfo.SPD + oCharacterInfo.DEF + oCharacterInfo.RES;
-		if (oCharacterInfo.RES) {
+		oCharacterInfo.BST = oCharacterInfo.Stats.HP + oCharacterInfo.Stats.ATK + oCharacterInfo.Stats.SPD + oCharacterInfo.Stats.DEF + oCharacterInfo.Stats.RES;
+		if (oCharacterInfo.Stats.RES) {
 			oSimplifiedInfo.push(oCharacterInfo);
 		}
 	});
@@ -104,28 +153,32 @@ function resetTemplate() {
 		"Attributes": {
 			"Name": ""
 		},
-		"HP": 0,
-		"ATK": 0,
-		"SPD": 0,
-		"DEF": 0,
-		"RES": 0,
+		"Stats": {
+			"HP": 0,
+			"ATK": 0,
+			"SPD": 0,
+			"DEF": 0,
+			"RES": 0
+		},
 		"BST": 0
 	};
 	return oTemplate;
 }
 
 // Visualises Stat vs. Number of Characters
-function createGraph(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
-	console.log(oInfo);
+function createGraph(oInfo) {
+	if (!oInfo) {
+		return 0;
+	}
 	var oContent = $("#idContent");
 
 	var sGraphType = getGraphType();
-	var sStatFilter = getStatFilter();
+	var aStatFilters = getStatFilters();
 	var aAttributeFilters = getAttributeFilters();
 
 	var margin = {
 		top: 20,
-		right: 30,
+		right: 60,		// 30
 		bottom: 50,		// 30
 		left: 60		// 40
 	},
@@ -142,22 +195,30 @@ function createGraph(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var oDataset = configureDataset(oInfo, sGraphType, sStatFilter, aAttributeFilters);
-	console.log(oDataset);
+	var oDataset = configureDataset(oInfo, sGraphType, aStatFilters, aAttributeFilters);
 
 	var xDomain = getXDomain(oDataset);
+	var yDomain = getYDomain(oDataset);
 	x.domain(xDomain);
-	y.domain([0, d3.max(oDataset, function (d) { return d.y; })]);
+	y.domain(yDomain);
 
-	oChart.selectAll(".bar")
-		.data(oDataset)
-		.enter().append("rect")
-		.attr("class", "bar")
-		.attr("x", function (d) { return x(d.x); })
-		.attr("y", function (d) { return y(d.y); })
-		.attr("height", function (d) { return height - y(d.y); })
-		.attr("width", x.bandwidth());
+	var iNumStatFilters = aStatFilters.length;
+	var oBarLength = x.bandwidth() / iNumStatFilters;
+	var oBars = oChart.selectAll(".bar")
+		.data(oDataset);
+	aStatFilters.forEach(function (sStatFilter, index) {
+		var dx = index * oBarLength;
+		oBars.enter().append("rect")
+			.attr("class", "bar " + sStatFilter)
+			.style("fill", oColours[sStatFilter])
+			.attr("x", function (d) { return x(d.x) + dx; })
+			.attr("y", function (d) { return y(d.y[sStatFilter]); })
+			.attr("height", function (d) { return height - y(d.y[sStatFilter]); })
+			.attr("width", oBarLength)
+	});
 
+	var xAxisText = getXAxisString(aStatFilters);
+	var axisFontSize = $(".axisHeading").css("font-size");
 	oChart.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
@@ -166,7 +227,7 @@ function createGraph(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
 		.attr("class", "axisHeading")
 		.attr("x", (width) / 2)
 		.attr("dy", margin.bottom / 2 + 18 / 2)		// TODO: Un-hardcode
-		.text(sStatFilter);
+		.text(xAxisText);
 
 	oChart.append("g")
 		.attr("class", "y axis")
@@ -179,47 +240,103 @@ function createGraph(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
 		.style("text-anchor", "end")
 		.text("Number of Heroes");
 
-	var axisFontSize = $(".axisHeading").css("font-size");
-	console.log(axisFontSize);
+}
+
+function getXAxisString(aStatFilters) {
+	var sAxisString = "";
+	for (var i = 0; i < aStatFilters.length; i++) {
+		switch(i) {
+			case 0:
+				sAxisString += aStatFilters[i];
+				break;
+			case (aStatFilters.length - 1):
+				sAxisString += " and " + aStatFilters[i];
+				break;
+			default:
+				sAxisString += ", " + aStatFilters[i];
+				break;
+		}
+	}
+	return sAxisString;
+}
+
+function getStatFilters() {
+	var aStatFilters = [];
+	var aStatCheckboxes = $(".stat");
+	for (var i = 0; i < aStatCheckboxes.length; i++) {
+		if (aStatCheckboxes[i].checked) {
+			aStatFilters.push(aStatCheckboxes[i].value);
+		}
+	}
+	return aStatFilters;
 }
 
 function getGraphType() {
 	return "Bar";
 }
 
-function getStatFilter() {
-	return "HP";
-}
-
 function getAttributeFilters() {
 	return [];
 }
 
-function configureDataset(oInfo, sGraphType, sStatFilter, aAttributeFilters) {
+function configureDataset(oInfo, sGraphType, aStatFilters, aAttributeFilters) {
+	var sStatFilter = aStatFilters[0];
 	var oDataset = [];
+	// Populate dataset to begin
 	oDataset.push({
-		x: oInfo[0][sStatFilter],
-		y: 0
+		x: oInfo[0]["Stats"][sStatFilter],
+		y: {
+			"HP": 1
+		}
 	});
 	switch(sGraphType) {
 		case "Bar":
 			oInfo.forEach(function (character) {
-				for (var i = 0; i < oDataset.length; i++) {
-					if(oDataset[i].x == character[sStatFilter]) {
-						oDataset[i].y++;
-						break;
-					} else if(i == oDataset.length - 1) {	// If not exists AND at end of loop
-						oDataset.push({
-							x: character[sStatFilter].toString(),
-							y: 0
-						});
+				Object.keys(character["Stats"]).forEach(function (stat) {
+					if (aStatFilters.indexOf(stat) >= 0) {
+						for (var i = 0; i < oDataset.length; i++) {
+							// If xValue exists and yValue exists
+							if (character["Stats"][stat] == oDataset[i].x && oDataset[i].y[stat]) {
+								oDataset[i].y[stat]++;
+								break;
+							}
+							// If xValue exists but yValue does not exist
+							else if (character["Stats"][stat] == oDataset[i].x) {
+								oDataset[i].y[stat] = 1;
+								break;
+							}
+							// If xValue does not exist and at the end of loop
+							else if (!(oDataset[i].x == character["Stats"][stat]) && i == oDataset.length - 1) {
+								oNewObj = {
+									x: character["Stats"][stat],
+									y: {}
+								};
+								oNewObj.y[stat] = 1;
+								oDataset.push(oNewObj);
+							}
+						}
 					}
-				}
+				});
 			});
 			return oDataset;
-		default:
-			break;
 	}
+}
+
+function changeStat(sStat) {
+	var aStatFilters = getStatFilters();
+	if (aStatFilters.length == 0) {
+		var oStatCheckboxes = $(".stat");
+		for (var i = 0; i < oStatCheckboxes.length; i++) {
+			if (oStatCheckboxes[i].value == sStat) {
+				oStatCheckboxes[i].checked = true;
+			}
+		}
+	}
+	redraw();
+}
+
+function changeAttribute() {
+
 }
 
 function getXDomain(oDataset) {
@@ -230,6 +347,26 @@ function getXDomain(oDataset) {
 	return aMap.sort();
 }
 
-function _redraw() {
-
+function getYDomain(oDataset) {
+	var minValue = 0;
+	var maxValue = 0;
+	oDataset.forEach(function (stat) {
+		Object.keys(stat.y).forEach(function (sStatFilter) {
+			if (stat.y[sStatFilter] > maxValue) {
+				maxValue = stat.y[sStatFilter];
+			}
+		});
+	})
+	return [minValue, maxValue];
 }
+
+function redraw() {
+	$("svg").remove();
+	createGraph(oFetchedInfo);
+	getStatFilters();
+}
+
+$(window).resize(function() {
+	$("svg").remove();
+	createGraph(oFetchedInfo);
+});
